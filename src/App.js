246572fe709709616b502c580d231c5594ecf521
blackbird <scofields109@gmail.com>
2023-01-29 12:41:32 +0100

    Front-end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import contact from './image/contact.png'
+import contactmail from './image/contact-mail.png'
+import call from './image/call.png'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className='sub-main'>
+        <div>
+          <div className='img'>
+            <div className='conntainer-image'>
+              <img src={contact} alt = 'profile image' className='contact'/>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
